c05d87c6cc793b31a95ecfdbfc7e330433147268
Iabianglang <iabianglang.sohkhlet@sonicbolt.com>
2022-07-13 08:53:41 +0530

    git

--- public/git_commands.sh
+++ public/git_commands.sh
@@ -1,19 +1,27 @@
+cd /c
+
+mkdir Client
+
 cd /c/Client
 
 git clone https://github.com/Iabianglang/Client_test.git
+
+cd /c/Client/Client_test
 
 git remote rm origin
 
 git filter-branch --subdirectory-filter dist -- --all
 
-mv * /c/server_test/public
+mv * /c/Server_test/public
 
-cd /c/server_test
+git add .
 
-git remote rm origin
+git commit -m "push"
+
+cd /c/Server_test
 
 git remote add origin https://github.com/Iabianglang/Server_test.git
 
-git pull origin master --allow-unrelated-histories
+git push origin main
 
 git status
